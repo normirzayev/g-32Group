45dc91708ec4f7a1cd55d2409e1aa2a1e9eabeb2
abduvali <97943192+normirzayev@users.noreply.github.com>
2023-06-15 17:40:40 +0500

    plus minus function

--- src/2-dars/Main.js
+++ src/2-dars/Main.js
@@ -1,18 +1,94 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Map from "./components/Map";
-
 export default function Main() {
-  let data = [
-    { id: 1, name: "islom" },
-    { id: 2, name: "karim" },
-    { id: 3, name: "olim" },
-    { id: 4, name: "mirza" },
-    { id: 5, name: "jasur" },
-  ];
+  let [data, setData] = useState([
+    { id: 1, name: "islom", soni: 0 },
+    { id: 2, name: "karim", soni: 0 },
+    { id: 3, name: "olim", soni: 0 },
+    { id: 4, name: "mirza", soni: 0 },
+    { id: 5, name: "jasur", soni: 0 },
+  ]);
+
+  let massivPlus = (parametr) => {
+    // console.log(parametr);
+
+    setData(
+      data.map((item) =>
+        item.id === parametr.id ? { ...item, soni: item.soni + 1 } : item
+      )
+    );
+  };
+  let massivMinus = (parametr) => {
+    // console.log(parametr);
+
+    setData(
+      data.map((item) => {
+        return item.id === parametr.id
+          ? { ...item, soni: item.soni - 1 }
+          : item;
+      })
+    );
+  };
+
+  // let handleClick = () => {
+  //   console.log("click ishlayapti ");
+  // };
+
+  // let handleFocus = () => {
+  //   console.log("focus ishlayapti");
+  // };
+
+  // let handleChange = () => {
+  //   console.log("change ishlayapti");
+  // };
+  // let handleKeyDown = (e) => {
+  //   console.log(e);
+  //   if (e.key === 2) {
+  //     alert("2 key bosildi");
+  //   }
+  // };
+
+  // o'zgaruvchini qiymatini oshirish
+
+  let [number, setNumber] = useState({ son: 0 });
+
+  // useEffect(() => {
+  //   setNumber(5);
+  // }, []);
+
+  // let number = 0;
+  let handlePlus = () => {
+    // number = number + 1
+    // number += 1
+    setNumber({ ...number, son: number.son + 1 });
+    console.log(number);
+  };
+
+  let handleMinus = () => {
+    setNumber({ ...number, son: number.son - 1 });
+  };
+
   return (
     <div>
       <h1>main</h1>
-      <Map data={data} />
+      <Map data={data} massivMinus={massivMinus} massivPlus={massivPlus} />
+
+      {/* <Map data={data} />
+      <button onDoubleClick={handleClick}> click me </button>
+      <input
+        type="checkbox"
+        onFocus={handleFocus}
+        // onInput={handleChange}
+        // onKeyUp={handleKeyDown}
+      /> */}
+      {/* <input type="text" name="ism" onChange={handlechange} />
+      <input type="text" name="email" onChange={handlechange} />
+      <input type="text" onChange={handlechange} name="addres" />
+      <input type="text" onChange={handlechange} name="number" />
+      <input type="text" onChange={handlechange} name="age" /> */}
+      {/* <button onClick={handleMinus}> minus </button>
+      <h1> son: {number.son} </h1>
+      <button onClick={handlePlus}> plus </button> */}
     </div>
   );
 }
--- src/2-dars/components/Map.js
+++ src/2-dars/components/Map.js
@@ -1,8 +1,21 @@
 import React from "react";
 
 export default function Map(prop) {
-  var { data } = prop;
-  console.log(data);
-
-  return <div>Map</div>;
+  var { data, massivPlus, massivMinus } = prop;
+  return (
+    <div>
+      {data.map((item) => {
+        return (
+          <div key={item.id}>
+            <h2>{item.name}</h2>
+            <button onClick={() => massivMinus(item)}>minus</button>
+            <h2>
+              mahsulotini soni {"=>"} {item.soni}{" "}
+            </h2>
+            <button onClick={() => massivPlus(item)}>plus</button>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
